89fe73663b58d4a0a813bc961c70f0ebd564bd0a
Felipe Carvalho <felipeemanuelc12@gmail.com>
2025-04-14 20:14:38 -0300

    User service

--- Library.Application/Services/UserService.cs
+++ Library.Application/Services/UserService.cs
@@ -1,8 +1,39 @@
-﻿using Library.Core.Interfaces.Services;
+﻿using Library.Core.Entities;
+using Library.Core.Interfaces.Repositories;
+using Library.Core.Interfaces.Services;
 
 namespace Library.Application.Services
 {
-    public class UserService : IUserService
+    public class UserService(IUserRepository _repository) : IUserService
     {
+        public async Task<IList<User>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task<User> CreateAsync(Book user)
+        {
+            await _repository.CreateAsync(book);
+            return user;
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            await _repository.UpdateAsync(book);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var user = await GetByIdAsync(id);
+            user.IsDeleted = true;
+            user.UpdateDate = DateTime.Now;
+
+            await _repository.UpdateAsync(user);
+        }
     }
 }
